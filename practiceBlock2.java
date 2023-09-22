4d1e6dd09353aff23a5f4875483b8fe81a73ed4d
AKuzminykh <hagen2006-75@yandex.ru>
2023-09-22 17:17:57 +0300

    Классы и методы. Задание 1

---
+++ practiceBlock2.java
@@ -0,0 +1,10 @@
+public class practiceBlock2 {
+
+    public static void main(String[] args){
+        System.out.println(fraction(3.5));
+    }
+    public static double fraction(double x){
+        int i=(int) x;
+        return x -i;
+    }
+}

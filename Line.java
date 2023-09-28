8bbb311e9d11449a802db58cbf7ee00efd6a6c87
AKuzminykh <hagen2006-75@yandex.ru>
2023-09-28 17:28:00 +0300

    Практика по разделу "ООП"

---
+++ Line.java
@@ -0,0 +1,4 @@
+public class Line {
+    Point p1;
+    Point p2;
+}
---
+++ Start.java
@@ -0,0 +1,11 @@
+public class Start {
+    public static void main(String[] args){
+        Point point1=new Point(1,3);
+        Point point2=new Point(1,3);
+        Point point3=new Point(5,8);
+        System.out.println(point1);
+        System.out.println(point2);
+        System.out.println(point3);
+        System.out.println(point1==point2);
+    }
+}
